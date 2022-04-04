04ac3987f9e7f7b0557df8fa45cb06c195df8894
vladwithcode <vladwithcode@gmail.com>
2022-04-04 13:04:37 -0500

    Possible fix for PROD AUTH

--- public/electron.js
+++ public/electron.js
@@ -1,9 +1,9 @@
-const electron = require("electron");
+const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require("path");
-const isDev = require("electron-is-dev");
+const path = require('path');
+const isDev = require('electron-is-dev');
 // const { protocol } = require('electron/main');
 
 let mainWindow;
@@ -13,14 +13,17 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 900,
     height: 680,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     minWidth: 900,
     minHeight: 680,
+    webPreferences: {
+      webSecurity: false,
+    },
   });
   mainWindow.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : path.join(relativePath, "build/index.html")
+      ? 'http://localhost:3000'
+      : path.join(relativePath, 'build/index.html')
   );
   if (isDev) {
     // Open the DevTools.
@@ -31,10 +34,35 @@ function createWindow() {
   //     mainWindow.webPreferences.devTools = false;
   // }
   mainWindow.setMenu(null);
-  mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.on('closed', () => (mainWindow = null));
+
+  mainWindow.webContents.session.webRequest.onBeforeSendHeaders(
+    (details, callback) => {
+      callback({
+        requestHeaders: {
+          Origin: 'http://dev.sibralan.com',
+          ...details.requestHeaders,
+        },
+      });
+    }
+  );
+
+  mainWindow.webContents.session.webRequest.onHeadersReceived(
+    (details, callback) => {
+      callback({
+        responseHeaders: {
+          'Access-Control-Allow-Origin': [
+            'http://main.sibralan.com',
+            'http://dev.sibralan.com',
+          ],
+          ...details.responseHeaders,
+        },
+      });
+    }
+  );
 }
 
-app.on("ready", () => {
+app.on('ready', () => {
   /* protocol.interceptFileProtocol('app', (req, cb) => {
         const url = req.url.substr(7);
         cb({
@@ -46,16 +74,16 @@ app.on("ready", () => {
   createWindow();
 });
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-console.log("electron-dev", app.getAppPath());
+console.log('electron-dev', app.getAppPath());
--- src/url.js
+++ src/url.js
@@ -1,7 +1,7 @@
-export const staticURL = 'http://main.sibralan.com:3000/api';
+export const staticURL = 'http://192.168.1.128:3000/api';
 
-// export const staticURL = 'http://localhost:3030/api';
+// export const staticURL = 'http://dev.sibralan.com:3030/api';
 
-export const staticURLDocs = 'http://main.sibralan.com:3000';
+export const staticURLDocs = 'http://192.168.1.128:3000';
 
-// export const staticURLDocs = 'http://localhost:3030';
+// export const staticURLDocs = 'http://dev.sibralan.com:3030';
